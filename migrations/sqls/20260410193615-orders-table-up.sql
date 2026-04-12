1eb8c7e36c33b8140220b8fa30fe26eff2479e3b
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2026-04-12 15:23:48 -0400

    fix: Change non-primary keys
    Change "productOrderId" and "userId" from "SERIAL PRIMARY KEY" to "bigint REFERENCES" to define them as non-primary keys to fix assertion error

--- migrations/sqls/20260410193615-orders-table-up.sql
+++ migrations/sqls/20260410193615-orders-table-up.sql
@@ -1,8 +1,8 @@
 /* Holds commands for Creating Table called orders */
 CREATE TABLE orders (
     id SERIAL PRIMARY KEY,
-    productOrderId SERIAL PRIMARY KEY,
+    productOrderId bigint REFERENCES products(id),
     quantity integer NOT NULL,
-    userId SERIAL PRIMARY KEY,
+    userId bigint REFERENCES users(id),
     orderStatus VARCHAR(64) NOT NULL CHECK (status IN ('active', 'complete'))
 );
