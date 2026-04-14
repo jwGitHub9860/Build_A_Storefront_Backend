5ba33ba9fccd95c3399f89012f0aa09f4e5f292d
jwGitHub9860 <113722079+jwGitHub9860@users.noreply.github.com>
2026-04-13 20:02:50 -0400

    feat: Add models for CRUD operations
    Add models for performing CRUD operationson databases

---
+++ src/models/product.ts
@@ -0,0 +1,71 @@
+// @ts-ignore
+import Client from "../database";
+import jwt from "jsonwebtoken";
+
+// Builds TypeScript "Product"
+export type Product = {
+    id: Number;
+    name: string;
+    price: number;
+    category: string;
+}
+
+// Build Out Methods for All CRUD Actions
+export class ProductStore {
+    // Obtains List of All Items in Database
+    // Method MUST Be Asynchronous Because All Calls to Database will be Promises
+    async index(): Promise<Product[]> {
+        // Protects from ANY ERRORS
+        try {
+            // @ts-ignore
+            const conn = await Client.connect()
+            const sql = 'SELECT * FROM products'
+            const result = await conn.query(sql)
+            conn.release()
+            return result.rows
+        } catch (err) {
+            throw new Error(`Cannot get products ${err}`);
+        }
+    }
+
+    async show(id: string): Promise<Product> {
+        try {
+            // @ts-ignore
+            const conn = await Client.connect()
+            const sql = 'SELECT * FROM products WHERE product=($1)'
+            const result = await conn.query(sql, [id])
+            conn.release()
+            return result.rows[0]
+        } catch (err) {
+            throw new Error(`Could not find product ${id}. Error: ${err}`);
+        }
+    }
+
+    async create(p: Product): Promise<Product> {
+        try {
+            // @ts-ignore
+            const conn = await Client.connect()
+            const sql = 'INSERT INTO products (name, price, category) VALUES ($1, $2, $3, $4) RETURNING *'
+            const result = await conn.query(sql, [p.name, p.price, p.category])
+            const product = result.rows[0]
+            conn.release()
+            return product
+        } catch (err) {
+            throw new Error(`Could not add new product ${p.name}. Error: ${err}`);
+        }
+    }
+
+    async delete(id: string): Promise<Product> {
+        try {
+            // @ts-ignore
+            const conn = await Client.connect()
+            const sql = 'DELETE FROM products WHERE id=($1)'
+            const result = await conn.query(sql, [id])
+            const product = result.rows[0]
+            conn.release()
+            return product
+        } catch (err) {
+            throw new Error(`Could not delete product ${id}. Error: ${err}`);
+        }
+    }
+}
---
+++ src/models/user.ts
@@ -0,0 +1,71 @@
+// @ts-ignore
+import Client from "../database";
+import jwt from "jsonwebtoken";
+
+// Builds TypeScript "User"
+export type User = {
+    id: Number,
+    firstName: string,
+    lastName: string,
+    password: string
+}
+
+// Build Out Methods for All CRUD Actions
+export class UserStore {
+    // Obtains List of All Items in Database
+    // Method MUST Be Asynchronous Because All Calls to Database will be Promises
+    async index(): Promise<User[]> {
+        // Protects from ANY ERRORS
+        try {
+            // @ts-ignore
+            const conn = await Client.connect()
+            const sql = 'SELECT * FROM users'
+            const result = await conn.query(sql)
+            conn.release()
+            return result.rows
+        } catch (err) {
+            throw new Error(`Cannot get orders ${err}`);
+        }
+    }
+
+    async show(id: string): Promise<User[]> {
+        try {
+            // @ts-ignore
+            const conn = await Client.connect()
+            const sql = 'SELECT * FROM users WHERE user=($1)'
+            const result = await conn.query(sql, [id])
+            conn.release()
+            return result.rows[0]
+        } catch (err) {
+            throw new Error(`Could not find user ${id}. Error: ${err}`);
+        }
+    }
+
+    async create(u: User): Promise<User> {
+        try {
+            // @ts-ignore
+            const conn = await Client.connect()
+            const sql = 'INSERT INTO users (firstName, lastName, password) VALUES ($1, $2, $3) RETURNING *'
+            const result = await conn.query(sql, [u.firstName, u.lastName, u.password])
+            const user = result.rows[0]
+            conn.release()
+            return user
+        } catch (err) {
+            throw new Error(`Could not add new user ${u.firstName}. Error: ${err}`);
+        }
+    }
+
+    async delete(id: string): Promise<User> {
+        try {
+            // @ts-ignore
+            const conn = await Client.connect()
+            const sql = 'DELETE FROM users WHERE id=($1)'
+            const result = await conn.query(sql, [id])
+            const user = result.rows[0]
+            conn.release()
+            return user
+        } catch (err) {
+            throw new Error(`Could not add new user ${id}. Error: ${err}`);
+        }
+    }
+}
